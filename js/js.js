b5ec966ccb3ef52e89829521c8ef44127d14c9ee
wwwangxinyi <51090503+wwwangxinyi@users.noreply.github.com>
2020-11-07 13:57:25 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,12 @@
+// function show_nav_header(){
+//     document.getElementById('nav_hover').style.display='inline';
+// }
+
+function show_nav(){
+    document.getElementById('full').style.opacity='1';
+    document.getElementById('full').style.display='block';
+}
+
+function show_page(){
+    document.getElementById('full').style.display='none';
+}
